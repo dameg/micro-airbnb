1e2bae5fdf5061ec4a9330ef1251d72a66bcc83f
Damian <damian@pureinteractive.pl>
2017-05-18 22:57:23 +0200

    server change

--- app.js
+++ app.js
@@ -87,6 +87,6 @@ app.use('/places/:id/comments', commentRoutes);
 //  /\__/ /  __/ |   \ V /  __/ |
 //  \____/ \___|_|    \_/ \___|_|
 
-app.listen(process.env.PORT, process.env.IP), function() {
+app.listen(process.env.PORT, process.env.IP, function () {
     console.log('Server started on localhost:3000');
 });
